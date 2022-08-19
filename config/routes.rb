de066c36efb3b13f4728ca28fd861676b73af749
JinDamanee <punnapob.jdk@gmail.com>
2022-08-20 01:22:52 +0700

    Finish Requirement

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/' =>'resume#index'
+  root 'resume#index'
+  # get '/' =>'resume#index'
+  
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
